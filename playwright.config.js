2cb99e01dc4fca84822fe4e81fdb96a1932afe3d
pkumv1 <45692629+pkumv1@users.noreply.github.com>
2025-05-19 11:52:06 +0530

    Add Playwright configuration

---
+++ playwright.config.js
@@ -0,0 +1,71 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './test_scripts/e2e',
+  
+  /* Maximum time one test can run for */
+  timeout: 30 * 1000,
+  
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  
+  /* Fail the build on CI if you accidentally left test.only in the source code */
+  forbidOnly: !!process.env.CI,
+  
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  
+  /* Opt out of parallel tests on CI */
+  workers: process.env.CI ? 1 : undefined,
+  
+  /* Reporter to use */
+  reporter: [
+    ['html', { outputFolder: './results/playwright-report' }],
+    ['json', { outputFile: './results/playwright-results.json' }]
+  ],
+  
+  /* Shared settings for all the projects below */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')` */
+    baseURL: 'https://www.saucedemo.com',
+    
+    /* Take screenshots on failure */
+    screenshot: 'only-on-failure',
+    
+    /* Collect trace when retrying the failed test */
+    trace: 'on-first-retry',
+  },
+  
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    
+    /* Test against mobile viewports */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+  ],
+  
+  /* Output folder for test artifacts like screenshots, videos, traces, etc. */
+  outputDir: './results/test-artifacts/',
+});
